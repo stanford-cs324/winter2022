0f2ac2a8ada6feb18b97a31458332647b7a8dbe4
Rishi Bommasani <rishibommasani@gmail.com>
2022-01-26 03:41:51 -0800

    Truncate test sets to 500 examples

--- assignments/p1/data.py
+++ assignments/p1/data.py
@@ -3,8 +3,14 @@ from datasets import load_dataset
 # Please do not change; we want to ensure all groups evaluate on the same data.
 seed = 324
 
-# We truncate the test set to 1k examples to expedite evaluation.
-test_size = 1000
+# We truncate the test set to expedite evaluation.
+test_size = 500
+
+
+def shuffle_and_truncate(dataset, size = 500, seed = 324):
+	dataset = dataset.shuffle(seed = seed)
+	dataset = dataset.select(list(range(size)))
+	return dataset
 
 
 def load_imdb():
@@ -12,15 +18,16 @@ def load_imdb():
 	dataset = train_dataset.train_test_split(test_size = test_size)
 	train_dataset, val_dataset = dataset['train'], dataset['test']
 	test_dataset = load_dataset('imdb', split = 'test')
-	test_dataset = test_dataset.shuffle(seed = seed)
-	test_dataset = test_dataset.select(list(range(test_size)))
+	test_dataset = shuffle_and_truncate(test_dataset, size = test_size, seed = seed)
 	return train_dataset, val_dataset, test_dataset
 
 
 def load_anli():
 	train_dataset = load_dataset('anli', split = 'train_r3')
 	val_dataset = load_dataset('anli', split = 'dev_r3')
+	val_dataset = shuffle_and_truncate(val_dataset, size = test_size, seed = seed)
 	test_dataset = load_dataset('anli', split = 'test_r3')
+	test_dataset = shuffle_and_truncate(test_dataset, size = test_size, seed = seed)
 	return train_dataset, val_dataset, test_dataset
 
 
