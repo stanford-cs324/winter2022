ea4962e649a084d34e355d8aa9a126b951088c2e
Percy Liang <percyliang@gmail.com>
2022-02-22 10:37:51 -0800

    update name

--- assignments/p2/scripts/cl-evaluate.sh
+++ assignments/p2/scripts/cl-evaluate.sh
@@ -10,7 +10,7 @@ eval_data=wordlength_eval_data
 seed=1111
 
 # Evaluate the GPT-2 model
-run_name="eval_${dataset_name}_step0"
+run_name="eval_${dataset_name}_seed${seed}_step0"
 cl run \
   --name $run_name \
   --request-docker-image sangxie513/cs324-p2-codalab-gpu \
@@ -21,7 +21,7 @@ cl run \
 
 # Evaluate on checkpoints of the continued-pretraining model.
 for step in 10000 20000 30000 40000 50000; do
-  run_name=eval_${dataset_name}_step${step}
+  run_name=eval_${dataset_name}_seed${seed}_step${step}
   train_dir=train_${dataset_name}_seed${seed}
   model_dir=${train_dir}/checkpoint-${step}
   cl run \
