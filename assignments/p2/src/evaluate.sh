c004ade8b2f8130ee072c9ca7008b14451543aea
Michael Xie <sangxie513@gmail.com>
2022-02-22 10:03:02 -0800

    eval script

--- assignments/p2/src/evaluate.sh
+++ assignments/p2/src/evaluate.sh
@@ -4,14 +4,13 @@ if [[ $# -lt 2 ]]; then
     echo "Main entry point for evaluation."
     echo "Usage:"
     echo
-    echo "    $0 <model_dir (e.g., train_openwebtext_wordlength_seed1111/checkpoint-10000> <eval_data_dir (e.g., wordlength_eval_data)> [additional arguments]"
+    echo "    $0 <model_dir (e.g., train_openwebtext_wordlength_seed1111/checkpoint-10000)> <eval_data_dir (e.g., wordlength_eval_data)> [additional arguments]"
     exit 1
+fi
 
-model_dir=$1;
-eval_data_dir=$2;
-shift
+model_dir=$1
+eval_data_dir=$2
 
 python src/evaluate_wordlength_model.py \
-    --model_dir ${MODEL_DIR} \
-    --eval_data_dir ${EVAL_DATA_DIR} \
-    "$@"
+    --model_dir ${model_dir} \
+    --eval_data_dir ${eval_data_dir}
