fdcd7b00381958259946673ea77c368ec5c26ed4
Michael Xie <sangxie513@gmail.com>
2022-02-22 09:53:43 -0800

    evaluate

--- assignments/p2/src/evaluate.sh
+++ assignments/p2/src/evaluate.sh
@@ -7,8 +7,9 @@ if [[ $# -lt 2 ]]; then
     echo "    $0 <model_dir (e.g., train_openwebtext_wordlength_seed1111/checkpoint-10000> <eval_data_dir (e.g., wordlength_eval_data)> [additional arguments]"
     exit 1
 
-model_dir=$1; shift
-eval_data_dir=$2; shift
+model_dir=$1;
+eval_data_dir=$2;
+shift
 
 python src/evaluate_wordlength_model.py \
     --model_dir ${MODEL_DIR} \
